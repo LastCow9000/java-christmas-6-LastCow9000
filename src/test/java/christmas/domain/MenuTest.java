52d6b99d6909fa8f7c31a635c0091ab9b77eb4f6
LastCow9000 <sys19912002@hanmail.net>
2023-11-12 19:19:26 +0900

    test(MenuTest): import수정

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -1,6 +1,6 @@
 package christmas.domain;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.exception.ExceptionMessage;
 import org.junit.jupiter.api.DisplayName;
