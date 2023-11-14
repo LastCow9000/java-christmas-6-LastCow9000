bdda1a73911e1e6d88aa0078a1e3814fb65cb707
LastCow9000 <sys19912002@hanmail.net>
2023-11-14 23:53:30 +0900

    feat: 할인 전 총주문 금액 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
     private static final String GREETING = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String PREVIEW_TITLE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDERED_MENU = "<주문 메뉴>";
+    private static final String TOTAL_BEFORE_DISCOUNT = "<할인 전 총주문 금액>";
 
     public static void printGreeting() {
         print(GREETING);
@@ -16,6 +17,11 @@ public class OutputView {
     public static void printOrderedMenu(String orders) {
         print(ORDERED_MENU);
         print(orders);
+    }
+
+    public static void printTotalBeforeDiscount(String amount) {
+        print(TOTAL_BEFORE_DISCOUNT);
+        print(amount);
     }
 
     private static void print(String input) {
