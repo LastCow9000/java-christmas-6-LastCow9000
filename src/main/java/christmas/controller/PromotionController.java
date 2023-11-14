c9039324c7394d28bdda499ba1915b08a8e32e40
LastCow9000 <sys19912002@hanmail.net>
2023-11-15 04:28:48 +0900

    refactor(PromotionController): 각 책임에 맞게 메서드 분리

--- src/main/java/christmas/controller/PromotionController.java
+++ src/main/java/christmas/controller/PromotionController.java
@@ -9,47 +9,101 @@ import christmas.domain.discount.GiftStrategy;
 import christmas.domain.discount.SpecialStrategy;
 import christmas.domain.discount.WeekdayStrategy;
 import christmas.domain.discount.WeekendStrategy;
+import christmas.exception.InputException;
 import christmas.util.CurrencyUtil;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class PromotionController {
 
 
     public void run() {
         OutputView.printGreeting();
-        Date date = Date.from(InputView.getDate());
 
-        Orders orders = new Orders(new ArrayList<>());
-        orders.createOrder(InputView.getMenu());
+        Date date = initVisitDate();
+        Orders orders = initOrders();
 
+        displayPreviewForEvent(date, orders);
+    }
+
+    private void displayPreviewForEvent(Date date, Orders orders) {
         OutputView.printPreviewTitle(date.getDay());
         OutputView.printOrderedMenu(orders.getStringOrders());
+        displayTotalBeforeDiscount(orders);
+        displayDiscountDetails(date, orders);
+    }
 
-        int amount = orders.calculateTotalBeforeDiscount();
-        OutputView.printTotalBeforeDiscount(CurrencyUtil.formatToKor(amount));
+    private void displayDiscountDetails(Date date, Orders orders) {
+        Discount discount = applyDiscount(date, orders);
 
+        OutputView.printGiftMenu(discount.getStringGiftMenu());
+        OutputView.printBenefitHistory(discount.getDetailedEventHistory());
+
+        int totalBenefitAmount = discount.getTotalBenefitAmount();
+        displayTotalBenefitAmount(totalBenefitAmount);
+        displayDiscountedTotal(orders, discount);
+
+        displayEventBadge(totalBenefitAmount);
+    }
+
+    private void displayEventBadge(int totalBenefitAmount) {
+        EventBadge eventBadge = EventBadge.getEventBadge(totalBenefitAmount);
+        OutputView.printEventBadge(eventBadge.getName());
+    }
+
+    private void displayDiscountedTotal(Orders orders, Discount discount) {
+        String amount = CurrencyUtil.formatToKor(
+                orders.calculateTotalBeforeDiscount() + discount.getTotalDiscountAmount());
+        OutputView.printDiscountedTotal(amount);
+    }
+
+    private void displayTotalBenefitAmount(int totalBenefitAmount) {
+        String totalBenefit = CurrencyUtil.formatToKor(totalBenefitAmount);
+        OutputView.printTotalBenefit(totalBenefit);
+    }
+
+    private void displayTotalBeforeDiscount(Orders orders) {
+        String totalAmount = CurrencyUtil.formatToKor(orders.calculateTotalBeforeDiscount());
+        OutputView.printTotalBeforeDiscount(totalAmount);
+    }
+
+    private Discount applyDiscount(Date date, Orders orders) {
         Discount discount = new Discount(List.of(
                 new DDayStrategy(),
                 new WeekdayStrategy(),
                 new WeekendStrategy(),
                 new SpecialStrategy(),
-                new GiftStrategy())
+                new GiftStrategy()),
+                date
         );
-        discount.checkEvent(orders, date);
-        OutputView.printGiftMenu(discount.getStringGiftMenu());
+        discount.checkEvent(orders);
 
-        OutputView.printBenefitHistory(discount.getDetailedEventHistory(date));
+        return discount;
+    }
 
-        int totalBenefitAmount = discount.getTotalBenefitAmount(date);
-        OutputView.printTotalBenefit(CurrencyUtil.formatToKor(totalBenefitAmount));
+    private Orders initOrders() {
+        return wrapByLoop(() -> {
+            Orders orders = new Orders(new ArrayList<>());
+            orders.createOrder(InputView.getMenu());
 
-        OutputView.printDiscountedTotal(
-                CurrencyUtil.formatToKor(
-                        orders.calculateTotalBeforeDiscount() + discount.getTotalDiscountAmount(date)));
+            return orders;
+        });
+    }
 
-        OutputView.printEventBadge(EventBadge.getEventBadge(totalBenefitAmount).getName());
+    private Date initVisitDate() {
+        return wrapByLoop(() -> Date.from(InputView.getDate()));
+    }
+
+    private <T> T wrapByLoop(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (InputException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 }
